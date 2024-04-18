5545e53b2cede0889f775ef3c0c84107fe1df96a
Gyuseok Lee <147016361+gyuseok0917@users.noreply.github.com>
2024-04-19 05:07:37 +0900

    s
    s

--- src/model/deepcnn.py
+++ src/model/deepcnn.py
@@ -7,28 +7,32 @@ class DeepCNN(nn.Module):
     def __init__(self):
         super().__init__()
         
-        # Encoder process
-        self.conv1 = nn.Conv2d(1, 64, kernel_size = (13, 5), stride = 2, padding = 1)
-        self.conv2 = nn.Conv2d(64, 64, kernel_size = (13, 5), stride = 2, padding = 1)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size = (13, 5), stride = 2, padding = 1)
+        # Encoder Module
+        self.en_module = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size = (13, 5), stride = 2, padding = 1),
+            nn.PReLU(),
+            nn.Conv2d(64, 64, kernel_size = (13, 5), stride = 2, padding = 1),
+            nn.PReLU(),
+            nn.Conv2d(64, 64, kernel_size = (13, 5), stride = 2, padding = 1),
+            nn.PReLU()
+        )
         
-        # Decoder process
-        self.deconv1 = nn.ConvTranspose2d(64, 64, kernel_size = (13, 9), stride = 2, padding = 1, output_padding = 1)
-        self.deconv2 = nn.ConvTranspose2d(64, 64, kernel_size = (13, 9), stride = 2, padding = 1, output_padding = 1)
-        self.deconv3 = nn.ConvTranspose2d(64, 64, kernel_size = (13, 9), stride = 2, padding = 1, output_padding = 1)
-
-        self.conv4 = nn.Conv2d(64, 64, kernel_size = (13, 5), stride = 1, padding = 1)
-        self.conv5 = nn.Conv2d(64, 1, kernel_size = (7, 1), stride = 1, padding = 1)
+        # Decoder Module
+        self.de_module = nn.Sequential(
+            nn.ConvTranspose2d(64, 64, kernel_size = (13, 9), stride = 2, padding = 1, output_padding = 1),
+            nn.PReLU(),
+            nn.ConvTranspose2d(64, 64, kernel_size = (13, 9), stride = 2, padding = 1, output_padding = 1),
+            nn.PReLU(),
+            nn.ConvTranspose2d(64, 64, kernel_size = (13, 9), stride = 2, padding = 1, output_padding = 1),
+            nn.PReLU(),
+            nn.Conv2d(64, 64, kernel_size = (13, 5), stride = 1, padding = 1),
+            nn.PReLU(),
+            nn.Conv2d(64, 1, kernel_size = (7, 1), stride = 1, padding = 1),
+            nn.PReLU()
+        )
 
     
     def forward(self, inputs):
-        out = F.prelu(self.conv1(inputs))
-        out = F.prelu(self.conv2(out))
-        out = F.prelu(self.conv3(out))
-        
-        out = F.prelu(self.deconv1(out))
-        out = F.prelu(self.deconv2(out))
-        out = F.prelu(self.deconv3(out))
-        out = F.prelu(self.conv4(out))
-        out = F.prelu(self.conv5(out))
+        out = self.en_module(inputs)    # Encoder Process
+        out = self.de_module(out)       # Decoder process
         return out
