17850ca0a98be665f1befd78c92976605530c3f5
Gyuseok Lee <147016361+gyuseok0917@users.noreply.github.com>
2024-04-19 04:53:29 +0900

    s
    s

--- gui_class/visualization.py
+++ gui_class/visualization.py
@@ -15,9 +15,6 @@ def eeg_plot(widget, eeg_data):
     # stack line plot 그리기
     for ch_idx in range(num_ch):
         widget.plot(np.arange(num_samples), eeg_data[ch_idx] + ch_idx * 3, pen = "b")
-    
-    # 축 범위 지정
-    widget.setRange(xRange = (0, 500), disableAutoRange=True)
 
     # Label 지정
     widget.setLabel("left", "Amplitude")
@@ -26,6 +23,6 @@ def eeg_plot(widget, eeg_data):
     # 눈금 수정
     channel_names = [f'Channel {i+1}' for i in range(num_ch)]
     widget.getAxis('left').setTicks([[(i * 3, name) for i, name in enumerate(channel_names)]])
-    widget.getAxis('bottom').setTicks([[(idx, f"{round(value, 2)}") for idx, value in zip(np.linspace(0, 500, 10), np.linspace(0, 1, 10))]])
+    widget.getAxis('bottom').setTicks([[(idx, f"{round(value, 2)}") for idx, value in zip(np.linspace(0, 500, 10), np.linspace(0, 0.5, 10))]])
 
     widget.showGrid(x = True, y = True)
