48efae4e73b6e0e409ed275a4a742615362b1c43
Lee Geonu <rjsdndl909@gmail.com>
2024-04-19 01:03:11 +0900

    First Commit 1

---
+++ gui_class/Upload.py
@@ -0,0 +1,28 @@
+
+from PyQt6 import QtWidgets, uic, QtCore, QtGui
+
+class UploadCompleteWindow(QtWidgets.QDialog):
+    def __init__(self):
+        super(UploadCompleteWindow, self).__init__()
+        uic.loadUi('uploadCompleteWindow.ui', self) 
+        self.confirmButton = self.findChild(QtWidgets.QPushButton, 'confirmButton') 
+        self.confirmButton.clicked.connect(self.close)
+
+class UploadWindow(QtWidgets.QDialog):  #업로드 창
+    def __init__(self):
+        super(UploadWindow, self).__init__()
+        uic.loadUi('uploadWindow.ui', self) 
+        self.lineEdit = self.findChild(QtWidgets.QLineEdit, 'lineEdit') 
+        self.lineEdit.mousePressEvent = self.openFileDialog
+        self.uploadButton = self.findChild(QtWidgets.QPushButton, 'uploadButton') 
+        self.uploadButton.clicked.connect(self.showUploadCompleteWindow)
+
+    def openFileDialog(self, event):
+        fname, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file', '/home')
+        if fname:
+            self.lineEdit.setText(fname) 
+
+    def showUploadCompleteWindow(self):
+        self.uploadCompleteWindow = UploadCompleteWindow()
+        self.uploadCompleteWindow.show()
+        self.uploadCompleteWindow.confirmButton.clicked.connect(self.close)
