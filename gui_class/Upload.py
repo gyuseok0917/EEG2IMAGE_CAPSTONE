9cd84386851ab6f55a80636b01d20e6cce891ce8
Lee Geonu <rjsdndl909@gmail.com>
2024-04-26 06:51:01 +0900

    0426

--- gui_class/Upload.py
+++ gui_class/Upload.py
@@ -1,29 +1,23 @@
+from PyQt6 import QtWidgets, uic, QtGui, QtCore
 
-from PyQt6 import QtWidgets, uic
-
-class UploadCompleteWindow(QtWidgets.QDialog):
-    def __init__(self):
-        super(UploadCompleteWindow, self).__init__()
-        uic.loadUi('./ui/uploadCompleteWindow.ui', self) 
-        self.confirmButton = self.findChild(QtWidgets.QPushButton, 'confirmButton') 
-        self.confirmButton.clicked.connect(self.close)
-
-class UploadWindow(QtWidgets.QDialog):  #업로드 창
+class UploadWindow(QtWidgets.QDialog):  # 업로드 창
     def __init__(self):
         super(UploadWindow, self).__init__()
-        uic.loadUi('./ui/uploadWindow.ui', self) 
-        self.lineEdit = self.findChild(QtWidgets.QLineEdit, 'lineEdit') 
+        uic.loadUi('./ui/uploadWindow.ui', self)
+        self.lineEdit = self.findChild(QtWidgets.QLineEdit, 'lineEdit')
         self.lineEdit.mousePressEvent = self.openFileDialog
-        self.uploadButton = self.findChild(QtWidgets.QPushButton, 'uploadButton') 
-        self.uploadButton.clicked.connect(self.showUploadCompleteWindow)
-
+        self.btn_UP_open = self.findChild(QtWidgets.QPushButton, 'btn_UP_open')
+        # displayImage 메소드를 버튼 클릭 시그널에 연결합니다.
+        self.filePath = ""  # 파일 경로를 저장할 변수 추가
+        self.btn_UP_open.clicked.connect(self.close)
+        #btn_UP_open이 눌렸을 때, self.close == 현재 클래스(창)을 닫음. 
+        # 업로드윈도우에 이 코드가 있으므로 이 창이 꺼짐.
     def openFileDialog(self, event):
         filters = "Image files (*.png *.jpg *.jpeg *.bmp);;All files (*.*)"
-        fname, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'OPEN', '/home', filters)
+        fname, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'OPEN', QtCore.QDir.homePath(), filters)
         if fname:
-            self.lineEdit.setText(fname) 
+            self.lineEdit.setText(fname)
+            self.filePath = fname  # 파일 경로 저장
+            
 
-    def showUploadCompleteWindow(self):
-        self.uploadCompleteWindow = UploadCompleteWindow()
-        self.uploadCompleteWindow.show()
-        self.uploadCompleteWindow.confirmButton.clicked.connect(self.close)
+          
--- main.py
+++ main.py
@@ -1,32 +1,50 @@
 import numpy as np
 import pyqtgraph as pg
-
 from PyQt6.QtWidgets import *
-from PyQt6.QtGui import QAction
-from PyQt6 import  uic
+from PyQt6 import  uic, QtGui, QtCore
 from gui_class import UploadWindow, eeg_plot
-
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
         uic.loadUi('./ui/main.ui', self)
+
         self.initUI()
         self.eeg_graph()
 
     def initUI(self):
-        upload_action = self.findChild(QPushButton, 'uploadButton') 
-        self.uploadButton.clicked.connect(self.showUploadWindow) 
-        
-    def showUploadWindow(self):
         self.uploadWindow = UploadWindow()
+        self.origin_image_frame = self.findChild(QFrame, 'origin_image_frame')
+
+        self.uploadButton.clicked.connect(self.showUploadWindow) 
+           # main.ui에 'uploadButton' 이 존재하기 때문에 findChidren 해줄 필요없음
+        
+
+           #uploadWindow 클래스(Upload.py)내에 존재하는 btn_UP_open 끌어옴. 
+
+    def showUploadWindow(self):
+        self.uploadWindow.btn_UP_open.clicked.connect(self.displayImage)
+        
         self.uploadWindow.show()
 
     def eeg_graph(self):
         eeg_widget = self.findChild(pg.PlotWidget, "graph")
         eeg_plot(eeg_widget, np.random.randn(8, 500))
-        
+
+    
+    def displayImage(self):  # 이미지 띄우기
+        if self.uploadWindow.filePath:  # 파일 경로가 있는 경우
+            
+            label = QLabel(self.origin_image_frame)  # 레이블 생성
+            pixmap = QtGui.QPixmap(self.uploadWindow.filePath)  # QPixmap 객체 생성
+            label.setPixmap(pixmap)  # 레이블에 QPixmap 객체 설정
+            label.setGeometry(0, 0, self.origin_image_frame.width(), self.origin_image_frame.height())  # 레이블의 위치와 크기 설정
+            label.setScaledContents(True)  # 이미지의 크기를 레이블 크기에 맞추어 조정   
+            label.show()  # 레이블 보이기
+
+
+
 if __name__ == "__main__":
     app = QApplication([])
     window = MainWindow()
