789ff017a7dc85c64f460aaaa8c2ad84990b5623
Lee Geonu <rjsdndl909@gmail.com>
2024-05-17 03:32:38 +0900

    프로그래스 바 / 슬라이드 이미지 업데이트

--- gui_class/Upload.py
+++ gui_class/Upload.py
@@ -1,12 +1,14 @@
 import mne
 import numpy as np 
+import base64
+import zlib
+import json
+import base64
 from mne.io.fiff.raw import Raw
 from PyQt6 import QtWidgets, uic, QtGui, QtCore
 from PyQt6.QtCore import pyqtSignal, QUrl
 from PyQt6.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
-import base64
-import zlib
-import json
+
 
 class UploadWindow(QtWidgets.QDialog):  # 업로드 창
     def __init__(self):
@@ -34,8 +36,104 @@ class UploadWindow(QtWidgets.QDialog):  # 업로드 창
 
 class UploadWindow_eeg(QtWidgets.QDialog):
     eegDataLoaded = pyqtSignal(Raw)
-    imageDataReceived = pyqtSignal(bytes)  # 이미지 데이터 수신 신호
+    imageDataReceived = pyqtSignal(list)  # list 타입으로 신호 정의
 
+    def __init__(self):
+        super(UploadWindow_eeg, self).__init__()
+        uic.loadUi('./ui/uploadWindow.ui', self)
+
+        self.filePath = ""
+        self.raw = None
+        self.response_bytes = None
+
+        self.lineEdit = self.findChild(QtWidgets.QLineEdit, 'lineEdit')
+        self.lineEdit.mousePressEvent = self.openFileDialog
+        self.EEG_upload_btn = self.findChild(QtWidgets.QPushButton, 'btn_UP_open')
+        self.EEG_upload_btn.clicked.connect(self.loadAndClose)
+        self.network_manager = QNetworkAccessManager(self)
+
+    def openFileDialog(self, event):
+        filters = "EEG file (*.fif);"
+        fname, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'EEG file Open', QtCore.QDir.homePath(), filters)
+
+        if fname:
+            self.lineEdit.setText(fname)
+            self.filePath = fname
+            self.loadAndClose()
+
+    def generation_visualization(self, number):
+        print("Image Number :", number)
+        data = self.raw.get_data()  
+        data_bytes = data.tobytes()
+        compressed_data = zlib.compress(data_bytes)
+        encoded_data = base64.b64encode(compressed_data).decode('utf-8')
+                
+        json_data = json.dumps({'eeg_data': encoded_data, "shape": data.shape, "number": number})
+        self.send_data(json_data)
+
+        self.eegDataLoaded.emit(self.raw)
+
+    def loadAndClose(self):
+        if self.filePath:
+            try:
+                self.raw = mne.io.read_raw_fif(self.filePath, preload=True)
+                self.raw = self.raw.pick_types(eeg=True)
+                if isinstance(self.raw, Raw):
+                    print("데이터 로드 중, 데이터 유형 확인 중.")
+                else:
+                    print("로드된 데이터는 Raw 객체가 아닙니다. 데이터 형식:", type(self.raw))
+            except Exception as e:
+                print("EEG 데이터 로딩 오류:", e)
+            finally:
+                self.close()
+
+    def send_data(self, json_data):
+        # 서버 URL 설정
+        url = QUrl('http://210.119.103.61:5000/transfer')
+        request = QNetworkRequest(url)
+        request.setHeader(QNetworkRequest.KnownHeaders.ContentTypeHeader, 'application/json')
+        
+        # 데이터 전송
+        reply = self.network_manager.post(request, json_data.encode('utf-8'))
+        reply.finished.connect(self.handle_response)
+
+    def handle_response(self):
+        reply = self.sender()
+        if reply.error() == QNetworkReply.NetworkError.NoError:
+            response_data = reply.readAll()
+            
+            if response_data:
+                self.response_bytes = bytes(response_data)
+                
+                try:
+                    json_data = json.loads(self.response_bytes)
+                    print("JSON Data:", json_data)  # JSON 데이터 출력하여 구조 확인
+                    
+                    image_list = json_data.get('images', [])
+                    
+                    self.image_paths = []
+                    for index, image_base64 in enumerate(image_list):
+                        print("Image Data:", image_base64)  # 확인용 출력
+                        
+                        image_bytes = base64.b64decode(image_base64)
+                        image_path = f"image_{index}.png"
+                        
+                        with open(image_path, 'wb') as image_file:
+                            image_file.write(image_bytes)
+                        
+                        self.image_paths.append(image_path)
+                    
+                    self.imageDataReceived.emit(self.image_paths)  # list 타입으로 emit
+                    
+                except json.JSONDecodeError:
+                    print("Failed to decode JSON data.")
+            else:
+                print("Received empty data from server.")
+        else:
+            print("Network error occurred:", reply.errorString())
+    eegDataLoaded = pyqtSignal(Raw)
+    # imageDataReceived = pyqtSignal(bytes)  # 이미지 데이터 수신 신호
+    imageDataReceived = QtCore.pyqtSignal(list)
     def __init__(self):
         super(UploadWindow_eeg, self).__init__()
         uic.loadUi('./ui/uploadWindow.ui', self)
@@ -43,6 +141,7 @@ class UploadWindow_eeg(QtWidgets.QDialog):
 
         self.filePath = ""
         self.raw = None
+        self.response_bytes = None
 
         self.lineEdit = self.findChild(QtWidgets.QLineEdit, 'lineEdit')
         self.lineEdit.mousePressEvent = self.openFileDialog
@@ -98,15 +197,69 @@ class UploadWindow_eeg(QtWidgets.QDialog):
         reply.finished.connect(self.handle_response)
 
     def handle_response(self):
-        reply = self.sender()
-        if reply.error() == QNetworkReply.NetworkError.NoError:
-            response_data = reply.readAll()
-            # 데이터 형식의 유효성을 확인하고 신호를 발생시킵니다.
-            if response_data:
-                # QByteArray를 bytes로 변환
-                response_bytes = bytes(response_data)
-                self.imageDataReceived.emit(response_bytes)  # 이미지 데이터 수신 신호 발생
+            reply = self.sender()
+            if reply.error() == QNetworkReply.NetworkError.NoError:
+                response_data = reply.readAll()
+                
+                if response_data:
+                    self.response_bytes = bytes(response_data)
+                    
+                    try:
+                        json_data = json.loads(self.response_bytes)
+                        print("JSON Data:", json_data)  # JSON 데이터 출력하여 구조 확인
+                        
+                        image_list = json_data.get('images', [])
+                        
+                        self.image_paths = []
+                        for index, image_base64 in enumerate(image_list):
+                            print("Image Data:", image_base64)  # 확인용 출력
+                            
+                            image_bytes = base64.b64decode(image_base64)
+                            image_path = f"image_{index}.png"
+                            
+                            with open(image_path, 'wb') as image_file:
+                                image_file.write(image_bytes)
+                            
+                            self.image_paths.append(image_path)
+                        
+                        self.imageDataReceived.emit(self.image_paths)  # list 타입으로 emit
+                        
+                    except json.JSONDecodeError:
+                        print("Failed to decode JSON data.")
+                else:
+                    print("Received empty data from server.")
             else:
-                print("Received empty data from server.")
-        else:
-            print("Network error occurred:", reply.errorString())
+                print("Network error occurred:", reply.errorString())
+            reply = self.sender()
+            if reply.error() == QNetworkReply.NetworkError.NoError:
+                response_data = reply.readAll()
+                
+                if response_data:
+                    self.response_bytes = bytes(response_data)
+                    
+                    try:
+                        json_data = json.loads(self.response_bytes)
+                        print("JSON Data:", json_data)  # JSON 데이터 출력하여 구조 확인
+                        
+                        image_list = json_data.get('images', [])
+                        
+                        self.image_paths = []
+                        for index, image_base64 in enumerate(image_list):
+                            print("Image Data:", image_base64)  # 확인용 출력
+                            
+                            image_bytes = base64.b64decode(image_base64)
+                            image_path = f"image_{index}.png"
+                            
+                            with open(image_path, 'wb') as image_file:
+                                image_file.write(image_bytes)
+                            
+                            self.image_paths.append(image_path)
+                        
+                        self.imageDataReceived.emit(self.image_paths)  # list 타입으로 emit
+                        
+                    except json.JSONDecodeError:
+                        print("Failed to decode JSON data.")
+                else:
+                    print("Received empty data from server.")
+            else:
+                print("Network error occurred:", reply.errorString())
--- main.py
+++ main.py
@@ -1,87 +1,147 @@
 import pyqtgraph as pg
 from mne.io.fiff.raw import Raw
 from PyQt6.QtWidgets import *
-from PyQt6 import  uic, QtGui
+from PyQt6 import uic, QtGui, QtCore
 from gui_class import UploadWindow, EEGPlotter, topomap, UploadWindow_eeg
 
-
-
 class MainWindow(QMainWindow):
+    imageDataReceived = QtCore.pyqtSignal(list)  # 클래스 정의 맨 위에 신호 정의
+
     def __init__(self):
         super(MainWindow, self).__init__()
-        uic.loadUi('./ui/main.ui', self)
-        self.initUI()
+        uic.loadUi('./ui/main.ui', self)  # UI 파일 로드
+        self.initUI()  # UI 초기화 함수 호출
+        self.nextBtn.setStyleSheet("QPushButton { opacity: 0.2; }")
+        self.prevBtn.setStyleSheet("QPushButton { opacity: 0.2; }")
 
-        
-
+    # UI 초기화 함수
     def initUI(self):
-        self.uploadWindow = UploadWindow()
-        self.eeg_uploadWindow = UploadWindow_eeg()
-        self.origin_image_frame = self.findChild(QFrame, 'origin_image_frame')  # Image
-        self.right_bottom_frame = self.findChild(QFrame, 'right_bottom_frame')  # Topomap
-        self.serverImageLabel = QLabel(self.findChild(QWidget, 'create_image_widget'))
-        self.serverImageLabel.setGeometry(0, 0, self.serverImageLabel.parent().width(), self.serverImageLabel.parent().height())
-        self.serverImageLabel.setScaledContents(True)
+        self.uploadWindow = UploadWindow()  # 업로드 창 인스턴스 생성
+        self.eeg_uploadWindow = UploadWindow_eeg()  # EEG 업로드 창 인스턴스 생성
+
+        # UI 요소들을 찾고 연결
+        self.origin_image_frame = self.findChild(QFrame, 'origin_image_frame')  # 원본 이미지를 표시할 프레임
+        self.right_bottom_frame = self.findChild(QFrame, 'right_bottom_frame')  # Topomap을 표시할 프레임
+        self.serverImageLabel = QLabel(self.findChild(QWidget, 'create_image_widget'))  # 서버에서 받은 이미지를 표시할 레이블
+        self.serverImageLabel.setGeometry(0, 0, self.serverImageLabel.parent().width(),
+                                          self.serverImageLabel.parent().height())  # 레이블 크기 설정
+        self.serverImageLabel.setScaledContents(True)  # 이미지 크기를 레이블에 맞추기
+        # 로딩 애니메이션 설정
+        self.loading_movie = QtGui.QMovie("loading.gif")
+        self.serverImageLabel.setMovie(self.loading_movie)
+        # 버튼 클릭 시 연결될 함수 설정
         self.uploadButton.clicked.connect(self.showUploadWindow)
         self.EEG_upload.clicked.connect(self.showUploadWindow_eeg)
-        self.eeg_uploadWindow.eegDataLoaded.connect(self.eeg_graph)
-        self.eeg_uploadWindow.imageDataReceived.connect(self.displayServerImage)  # 이미지 데이터 수신 신호 연결
+        self.eeg_uploadWindow.eegDataLoaded.connect(self.eeg_graph)  # EEG 데이터 로드 신호 연결
+        self.eeg_uploadWindow.imageDataReceived.connect(self.update_image_paths)  # 이미지 데이터 수신 신호 연결
         self.startButton.clicked.connect(self.handle_start)
 
+        self.image_paths = []
+        self.current_index = 0
+        # 생성 이미지 이전 버튼     
+        self.prevBtn.clicked.connect(self.show_prev_image)    
+        # 생성 이미지 다음 버튼 
+        self.nextBtn.clicked.connect(self.show_next_image) 
+        # 이미지 수신 신호 연결
+        self.imageDataReceived.connect(self.update_image_paths)
 
+    def update_image_paths(self, paths):
+        self.image_paths = paths
+        if self.image_paths:
+            self.displayServerImage(self.image_paths[0])
+
+    def displayServerImage(self, image_path):
+        self.loading_movie.stop()  # 로딩 애니메이션 중지
+        self.startButton.setEnabled(True)  # 버튼 활성화
+
+        pixmap = QtGui.QPixmap()  # QPixmap 객체 생성
+        pixmap.load(image_path)  # 이미지 파일 경로를 사용하여 QPixmap에 로드
+        self.serverImageLabel.setPixmap(pixmap)  # 레이블에 QPixmap 설정
+        self.serverImageLabel.show()  # 레이블 표시
+
+    def show_next_image(self):
+        if self.image_paths:
+            self.current_index = (self.current_index + 1) % len(self.image_paths)
+            self.displayServerImage(self.image_paths[self.current_index])
+
+    def show_prev_image(self):
+        if self.image_paths:
+            self.current_index = (self.current_index - 1) % len(self.image_paths)
+            self.displayServerImage(self.image_paths[self.current_index])
+
+    def enterEvent(self, event):
+        self.prevBtn.setStyleSheet("QPushButton { opacity: 1.0; }")
+        self.nextBtn.setStyleSheet("QPushButton { opacity: 1.0; }")
+        super().enterEvent(event)
+
+    def leaveEvent(self, event):
+        self.prevBtn.setStyleSheet("QPushButton { opacity: 0.2; }")
+        self.nextBtn.setStyleSheet("QPushButton { opacity: 0.2; }")
+        super().leaveEvent(event)
+
+    # 시작 버튼 클릭 시 실행될 함수
     def handle_start(self):
-        number = self.ImageNumber.value()
-        self.eeg_uploadWindow.generation_visualization(number)
-        
+        self.startButton.setEnabled(False)  # 버튼 비활성화
+        number = self.ImageNumber.value()  # UI에서 설정한 이미지 번호 가져오기
+        self.eeg_uploadWindow.generation_visualization(number)  # 시각화 생성 함수 호출
+        self.loading_movie.start()  # 로딩 애니메이션 시작
+        print("start loading")
+
+    # 이미지 업로드 창 표시 함수
     def showUploadWindow(self):
-        self.uploadWindow.btn_UP_open.clicked.connect(self.displayImage)
-        self.uploadWindow.show()
+        self.uploadWindow.btn_UP_open.clicked.connect(self.displayImage)  # 업로드 버튼 클릭 시 displayImage 함수 연결
+        self.uploadWindow.show()  # 업로드 창 표시
 
-
+    # EEG 업로드 창 표시 함수
     def showUploadWindow_eeg(self):
-        # 기존에 연결된 모든 신호를 제거하고 새로 연결 (여러 번 창을 열 때 중복 연결을 방지)
+        # 기존에 연결된 모든 신호를 제거하고 새로 연결 (중복 연결 방지)
         self.eeg_uploadWindow.EEG_upload_btn.clicked.disconnect()
         self.eeg_uploadWindow.EEG_upload_btn.clicked.connect(self.eeg_uploadWindow.openFileDialog)
         self.eeg_uploadWindow.eegDataLoaded.connect(self.eeg_graph)
-        self.eeg_uploadWindow.show()
+        self.eeg_uploadWindow.show()  # EEG 업로드 창 표시
 
-
-    def displayImage(self):  # 이미지 띄우기
+    # 업로드된 이미지 표시 함수
+    def displayImage(self):
         if self.uploadWindow.filePath:  # 파일 경로가 있는 경우
-            
             label = QLabel(self.origin_image_frame)  # 레이블 생성
             pixmap = QtGui.QPixmap(self.uploadWindow.filePath)  # QPixmap 객체 생성
             label.setPixmap(pixmap)  # 레이블에 QPixmap 객체 설정
-            label.setGeometry(0, 0, self.origin_image_frame.width(), self.origin_image_frame.height())  # 레이블의 위치와 크기 설정
-            label.setScaledContents(True)  # 이미지의 크기를 레이블 크기에 맞추어 조정   
-            label.show()  # 레이블 보이기
+            label.setGeometry(0, 0, self.origin_image_frame.width(), self.origin_image_frame.height())  # 레이블 크기 설정
+            label.setScaledContents(True)  # 이미지 크기를 레이블에 맞추기
+            label.show()  # 레이블 표시
 
-    def displayServerImage(self, image_data):
-        pixmap = QtGui.QPixmap()
-        pixmap.loadFromData(image_data)
-        self.serverImageLabel.setPixmap(pixmap)
-        self.serverImageLabel.show()
+    # 서버에서 받은 이미지 표시 함수
+    def displayServerImage(self, image_path):
+        self.loading_movie.stop()  # 로딩 애니메이션 중지
+        print("stop loading")
+        self.startButton.setEnabled(True)  # 버튼 활성화
 
+        pixmap = QtGui.QPixmap()  # QPixmap 객체 생성
+        pixmap.load(image_path)  # 이미지 파일 경로를 사용하여 QPixmap에 로드
+        self.serverImageLabel.setPixmap(pixmap)  # 레이블에 QPixmap 설정
+        self.serverImageLabel.show()  # 레이블 표시
+
+    # EEG 그래프 표시 함수
     def eeg_graph(self, raw_eeg):
-        if isinstance(raw_eeg, Raw):
-            eeg_widget = self.findChild(pg.PlotWidget, "graph")
-            self.plotter = EEGPlotter(eeg_widget, raw_eeg)
-            self.plotter.regionChanged.connect(self.topo_show)
+        if isinstance(raw_eeg, Raw):  # 전달된 데이터가 Raw 객체인 경우
+            eeg_widget = self.findChild(pg.PlotWidget, "graph")  # 그래프를 표시할 위젯 찾기
+            self.plotter = EEGPlotter(eeg_widget, raw_eeg)  # EEGPlotter 객체 생성
+            self.plotter.regionChanged.connect(self.topo_show)  # 영역 변경 신호에 topo_show 함수 연결
         else:
             print("Main.py Alert : eeg_graph에 전달된 데이터는 Raw 객체가 아닙니다. 수신된 데이터 유형:", type(raw_eeg))
 
-
+    # Topomap 표시 함수
     def topo_show(self, tmin, tmax):
-        image = topomap(self.plotter.raw_eeg, tmin, tmax)
-        label = QLabel(self.right_bottom_frame)
-        label.setPixmap(image)
-        label.setGeometry(0, 0, self.right_bottom_frame.width(), self.right_bottom_frame.height())
-        label.setScaledContents(True)
-        label.show()
+        image = topomap(self.plotter.raw_eeg, tmin, tmax)  # Topomap 이미지 생성
+        label = QLabel(self.right_bottom_frame)  # 레이블 생성
+        label.setPixmap(image)  # 레이블에 Topomap 이미지 설정
+        label.setGeometry(0, 0, self.right_bottom_frame.width(), self.right_bottom_frame.height())  # 레이블 크기 설정
+        label.setScaledContents(True)  # 이미지 크기를 레이블에 맞추기
+        label.show()  # 레이블 표시
 
-
+# 메인 함수
 if __name__ == "__main__":
-    app = QApplication([])
-    window = MainWindow()
-    window.show()
-    app.exec()
+    app = QApplication([])  # QApplication 객체 생성
+    window = MainWindow()  # MainWindow 객체 생성
+    window.show()  # MainWindow 표시
+    app.exec()  # 애플리케이션 실행
