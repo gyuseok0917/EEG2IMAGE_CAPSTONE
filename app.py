fbd97886bc258b33948223579a00b3597ab3500e
Gyuseok Lee <147016361+gyuseok0917@users.noreply.github.com>
2024-05-24 07:36:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import time
 import json
 import base64
 import zlib
+import mne
+import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -47,8 +49,6 @@ class ImageGenProcess:
         # Flask app operation
         self.flask_runs()
         
-        self.eeg_data = None
-        
         
     def get_eeg(self):
         """
@@ -57,17 +57,17 @@ class ImageGenProcess:
         
         json_data = request.get_json()
         encoded_data = json_data['eeg_data'] # Encoded eeg data
-        data_shape = json_data["shape"]      # EEG data's shape
-        compressed_data = base64.b64decode(encoded_data) # Decode base64 format
-        data_bytes = zlib.decompress(compressed_data) # Restore compressed data
+        raw_bytes = base64.b64decode(encoded_data) # Decode base64 format
+        # data_bytes = zlib.decompress(compressed_data) # Restore compressed data
         
         # Convert byte form to numpy matrix (reshape to size of original data)
-        data_array = np.frombuffer(data_bytes, dtype=np.float64).reshape(data_shape)
+        raw = pickle.loads(raw_bytes)
+        # data_array = np.frombuffer(data_bytes, dtype=np.float64).reshape(data_shape)
         
-        print(f"Get EEG data: {data_array.shape}")
+        print(f"Get EEG data: {raw.get_data().shape}")
         print(f"Get Number: {json_data['number']}")
         
-        return data_array
+        return raw
         
     
     def generation_process(self):
@@ -77,8 +77,6 @@ class ImageGenProcess:
         
         eeg_data = self.get_eeg() # Get eeg data
         image = self.model(eeg_data) # Model operation
-        
-        self.eeg_data = eeg_data
 
         return eeg_data, image
     
@@ -89,28 +87,24 @@ class ImageGenProcess:
         """
         
         # Image Generation
-        eeg_data, image = self.generation_process()
+        _, image = self.generation_process()
         
-        eeg_encoded = convert_byte_ee(eeg_data)        
+        # SR 대신 Server의 128-ch EEG를 송신
+        eeg_data = mne.io.read_raw_fif("sample_eeg.fif", preload = True)
+        eeg_data = eeg_data.pick_types(eeg = True)
+        
+        eeg_encoded = self.convert_byte_eeg(eeg_data)        
         image_byte = [self.convert_byte_image(img) for img in image]
         
-        return jsonify({"eeg_data": eeg_encoded, "images": image_byte, "shape": eeg_data.shape})
-    
-    
-    def send_select_ch_eeg(self):
-        
-        json_data = request.get_json()
-        n_ch = json_data['n_ch']
-        
-        select_ch_eeg = self.eeg_data[:, :n_ch]
-        return jsonify({"eeg_data": select_ch_eeg, "shape": select_ch_eeg.shape})
+        return jsonify({"eeg_data": eeg_encoded, "images": image_byte})
         
         
-    def convert_byte_eeg(self, eeg_data):
-        eeg_bytes = eeg_data.tobytes()
-        eeg_compressed = zlib.compress(eeg_bytes)
-        eeg_encoded = base64.b64encode(eeg_compressed).decode('utf-8')
-        return eeg_encoded     
+    def convert_byte_eeg(self, raw_eeg):
+        raw_bytes = pickle.dumps(raw_eeg)
+        # eeg_bytes = eeg_data.tobytes()
+        # eeg_compressed = zlib.compress(eeg_bytes)
+        raw_base64 = base64.b64encode(raw_bytes).decode('utf-8')
+        return raw_base64     
     
     
     def convert_byte_image(self, image):
@@ -126,7 +120,6 @@ class ImageGenProcess:
         Specify the URL path to receive the client's request
         """
         self.app.add_url_rule('/transfer', 'transfer', self.send_image, methods=['POST'])
-        self.app.add_url_rule('/select-ch', 'select-ch', self.send_select_ch_eeg, methods=['POST'])
         
     
     def flask_runs(self):
