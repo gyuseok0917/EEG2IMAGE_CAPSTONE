96b5b0e6ca0e9010e518b2d9a032caaa8df2ff7f
Gyuseok Lee <147016361+gyuseok0917@users.noreply.github.com>
2024-05-23 18:55:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 import json
 import base64
 import zlib
@@ -5,8 +6,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from io import BytesIO
+from glob import glob
 from PIL import Image
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify
 
 
 class Model:
@@ -17,7 +19,10 @@ class Model:
         pass
     
     def __call__(self, inputs):
-        image = Image.open("image/tiger.jpg")
+        image = []
+        
+        for path in glob("image/*.jpg"):
+            image.append(Image.open(path))
         return image
 
 
@@ -42,6 +47,8 @@ class ImageGenProcess:
         # Flask app operation
         self.flask_runs()
         
+        self.eeg_data = None
+        
         
     def get_eeg(self):
         """
@@ -55,8 +62,10 @@ class ImageGenProcess:
         data_bytes = zlib.decompress(compressed_data) # Restore compressed data
         
         # Convert byte form to numpy matrix (reshape to size of original data)
-        data_array = np.frombuffer(data_bytes, dtype=np.float32).reshape(data_shape)
-        print(data_array.shape)
+        data_array = np.frombuffer(data_bytes, dtype=np.float64).reshape(data_shape)
+        
+        print(f"Get EEG data: {data_array.shape}")
+        print(f"Get Number: {json_data['number']}")
         
         return data_array
         
@@ -68,8 +77,10 @@ class ImageGenProcess:
         
         eeg_data = self.get_eeg() # Get eeg data
         image = self.model(eeg_data) # Model operation
+        
+        self.eeg_data = eeg_data
 
-        return image
+        return eeg_data, image
     
     
     def send_image(self):
@@ -78,14 +89,36 @@ class ImageGenProcess:
         """
         
         # Image Generation
-        image = self.generation_process()
+        eeg_data, image = self.generation_process()
         
-        # Save image in byte form
+        eeg_encoded = convert_byte_ee(eeg_data)        
+        image_byte = [self.convert_byte_image(img) for img in image]
+        
+        return jsonify({"eeg_data": eeg_encoded, "images": image_byte, "shape": eeg_data.shape})
+    
+    
+    def send_select_ch_eeg(self):
+        
+        json_data = request.get_json()
+        n_ch = json_data['n_ch']
+        
+        select_ch_eeg = self.eeg_data[:, :n_ch]
+        return jsonify({"eeg_data": select_ch_eeg, "shape": select_ch_eeg.shape})
+        
+        
+    def convert_byte_eeg(self, eeg_data):
+        eeg_bytes = eeg_data.tobytes()
+        eeg_compressed = zlib.compress(eeg_bytes)
+        eeg_encoded = base64.b64encode(eeg_compressed).decode('utf-8')
+        return eeg_encoded     
+    
+    
+    def convert_byte_image(self, image):
         image_bytes = BytesIO()
         image.save(image_bytes, "JPEG", quality = 70)
         image_bytes.seek(0)
-        
-        return send_file(image_bytes, mimetype = "image/jpeg", as_attachment=True, download_name = 'filename.jpg')
+        image_base64 = base64.b64encode(image_bytes.getvalue()).decode('utf-8')
+        return image_base64
         
     
     def add_routes(self):
@@ -93,6 +126,7 @@ class ImageGenProcess:
         Specify the URL path to receive the client's request
         """
         self.app.add_url_rule('/transfer', 'transfer', self.send_image, methods=['POST'])
+        self.app.add_url_rule('/select-ch', 'select-ch', self.send_select_ch_eeg, methods=['POST'])
         
     
     def flask_runs(self):
